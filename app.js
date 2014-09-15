499a1e11a0a85859db72dab39cc61c728edbed4f
Aaron Hinojosa <aaron@appcard.com>
2014-09-15 15:03:05 -0400

    boilerplate for stub api

---
+++ app.js
@@ -0,0 +1,58 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var app = express();
+var router;
+var port = process.env.PORT | 4000;
+
+// all environments
+app.use(bodyParser());
+
+router = express.Router();  // get instance of express router
+
+// add middleware to router
+router.use(function(req, res, next) {
+    // logging, stats, etc. here
+    // add optional authentication
+    next();
+});
+
+router.get('/', function(req, res) {
+    res.json({message: 'server is up and running'});
+}); 
+
+/**
+ *
+ * additional routes
+ *
+ */
+
+// Post route
+router.route('/user')
+
+    .post(function(req, res) {
+        res.json({
+            "data": {
+                "user": "Some User",
+                "": "Silver"
+            }
+        });
+    });
+
+
+// Get route
+router.route('/version')
+
+    .get(function(req, res) {
+        res.json({
+            "version": 1
+        });
+    });
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Running on port: ' + port);
